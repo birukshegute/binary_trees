2084ac63b01c60028322e7c9544837a2207181b3
birukshegute <birukshegute@gmail.com>
2024-01-04 00:57:58 -0800

    task 0 trial 3

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
-/***
- * binary_tree_node - Create a new node for binary tree
+/**
+ * binary_tree_node - Create a new node for binary tree.
  * @parent: parent node
  * @value: value of parent node
  *
- * Return - new node
+ * Return: new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -16,11 +16,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (newNode == NULL)
 	{
 		return (NULL);
-	}	
+	}
 	newNode->n = value;
 	newNode->parent = parent;
 	newNode->left = NULL;
 	newNode->right = NULL;
 
-	return newNode;
+	return (newNode);
 }
