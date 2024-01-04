04b18b8176aca7b27a99efeeb11e2aef7d8a7c6d
birukshegute <birukshegute@gmail.com>
2024-01-04 01:29:28 -0800

    task trial 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,12 @@
+#include "binary_sort_h"
+
+/*
+* binary_tree_insert_left - inserts a node as the left-child of another node
+* parent: pointer to the node to insert the left-child in
+* value: is the value to store in the new node 
+*
+* Return: 0
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+
