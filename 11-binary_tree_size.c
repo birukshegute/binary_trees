6e7a2afc78d9e7f6f193a1a04f66d9ffb685d397
birukshegute <birukshegute@gmail.com>
2024-01-05 12:22:56 -0800

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: a pointer to the root node of the tree
- * 
+ *
  * Return: size of tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
