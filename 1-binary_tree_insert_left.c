da3d1a2c9770d1db06d24651419ebb7c5be5073d
birukshegute <birukshegute@gmail.com>
2024-01-04 17:46:56 -0800

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,4 @@
-#include "binary_sort_h"
+#include "binary_trees.h"
 
 /*
 * binary_tree_insert_left - inserts a node as the left-child of another node
@@ -9,5 +9,23 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
+	}
+	parent->left = newNode;
+	return (newNode);	
 }
