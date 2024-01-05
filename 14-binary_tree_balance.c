e67434f36299a821e093693b5f91ac4d326fe9b5
birukshegute <birukshegute@gmail.com>
2024-01-05 13:24:11 -0800

    task 15

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,11 @@
+#include "binary_trees"
+/**
+ * binary_tree_balance - measures the balance factor of a function
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: balance factor of the tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	
+}
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full -  a function that checks if a tree is full
+ * @tree: pointer to the root of the tree
+ *
+ * Return: 1 if full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+	if (!tree->left || !tree->right)
+		return (0);
+
+	return (binary_tree_is_full(tree->left) &&
+		binary_tree_is_full(tree->right));
+
+}
