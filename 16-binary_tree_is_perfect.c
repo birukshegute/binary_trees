8078ba104b00a092b3cbf27c3cceca2056a966ca
birukshegute <birukshegute@gmail.com>
2024-01-05 13:40:06 -0800

    task 18

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,12 @@
+#include "binary_tress.h"
+
+/**
+ * binary_tree_is_perfect - checks if a tree is perfect
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 1 if perfect
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	
+}
